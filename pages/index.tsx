56fa5b71a4533d6ceb4495331f189a9824ed345e
Avery King <avery.n.king@wmich.edu>
2022-03-20 03:01:05 -0400

    ``Post`` interface created

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,10 @@ import Image from 'next/image'
 import Header from "../components/Header";
 import {sanityClient, urlFor} from '../sanity';
 
-const Home: NextPage = () => {
+interface props {
+    posts: Post[];
+}
+const Home: NextPage = (props: Props) => {
     return (
         <div className='max-w-7xl mx-auto'>
             <Head>
