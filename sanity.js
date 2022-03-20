370d360aa1d53cd0ebb9f4fddbb7d570c2f1c989
Avery King <avery.n.king@wmich.edu>
2022-03-20 02:18:01 -0400

    ``.env.local`` added

--- sanity.js
+++ sanity.js
@@ -8,5 +8,5 @@ export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: "2021-03-25",
-    useCdn: process.enc.NODE_ENV === "production"
+    useCdn: process.env.NODE_ENV === "production"
 };
