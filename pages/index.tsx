ffaf4e8e08134aaf34e15bf75a7ac5d38e747b91
Avery King <avery.n.king@wmich.edu>
2022-03-20 02:56:52 -0400

    posts fetched from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
+import {sanityClient, urlFor} from '../sanity';
 
 const Home: NextPage = () => {
     return (
@@ -12,12 +13,14 @@ const Home: NextPage = () => {
             </Head>
             <Header/>
             <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
-            <div className='px-10 space-y-5'>
-                    <h1 className='text-6xl max-w-xl font-serif'><span className='underline decoration-black decoration-4'>Medium</span> is a place to write, read, and connect.</h1>
+                <div className='px-10 space-y-5'>
+                    <h1 className='text-6xl max-w-xl font-serif'><span
+                        className='underline decoration-black decoration-4'>Medium</span> is a place to write, read, and
+                        connect.</h1>
                     <h2>It's easy and free to post your thinking on any topic and connect with millions of readers.</h2>
                 </div>
                 <img className='hidden md:inline-flex h-32 lg:h-full'
-                    src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+                     src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
                      alt="Medusa"/>
             </div>
             { /* Posts */}
@@ -27,6 +30,24 @@ const Home: NextPage = () => {
 
 export default Home
 
-export const getServerSideProps = async() => {
-    const query = null;
+export const getServerSideProps = async () => {
+    const query = `*[_type == "post"] {
+      _id,
+      title,
+      author -> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+    }`;
+    const posts = await sanityClient.fetch(query);
+
+    return {
+        props: {
+            posts
+        }
+    }
 }
+
