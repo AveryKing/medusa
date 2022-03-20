8d4dbc12cbccac5aa7d1850eb57c54cfe668e5e7
Avery King <avery.n.king@wmich.edu>
2022-03-20 15:11:17 -0400

    submit handler added to comment form

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -18,8 +18,11 @@ interface IFormInput {
 }
 
 const Post = ({post}: Props) => {
-
     const {register, handleSubmit, formState: {errors}} = useForm<IFormInput>();
+
+    const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+        console.log(data)
+    };
     return (
         <main>
             <Header/>
@@ -64,7 +67,8 @@ const Post = ({post}: Props) => {
 
             </article>
             <hr className="max-w-lg my-5 mx-auto border border-blue-500"/>
-            <form className="flex flex-col p-5 my-10 max-w-2xl mx-auto mb-10">
+            <form onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col p-5 my-10 max-w-2xl mx-auto mb-10">
                 <h3 className="text-sm text-blue-500">Enjoyed this article?</h3>
                 <h4 className="text-3xl font-bold">Leave a comment below!</h4>
                 <hr className="py-3 mt-2"/>
