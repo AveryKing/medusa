c460184ba65f9df61bdc4080cab84d99a13b0bac
Avery King <avery.n.king@wmich.edu>
2022-03-20 04:32:44 -0400

    title and author info rendered on post page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "../../components/Header";
-import {sanityClient} from "../../sanity";
+import {sanityClient, urlFor} from "../../sanity";
 import {Post} from "../../typings";
 import { GetStaticProps} from "next";
 
@@ -8,10 +8,26 @@ interface Props {
     post: Post;
 }
 const Post = ({post}: Props) => {
-    console.log(post)
+
     return (
         <main>
             <Header/>
+            <img className='w-full h-40 object-cover'
+                src={urlFor(post.mainImage).url()}
+                 alt={post.title}
+                 />
+            <article className='max-w-3xl mx-auto p-5'>
+                <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+                <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
+                <div className="flex items-center space-x-2">
+                    <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url()!}
+                         alt={post.author.name} />
+                    <p className="font-extralight text-sm">
+                        Blog post by <span className="text-green-600">{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}
+
+                    </p>
+                </div>
+            </article>
         </main>
     );
 };
